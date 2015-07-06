496d8928033884ab260f7c92335c204b1eeab6b5
case <case@casesandberg.com>
2015-07-06 16:07:27 -0700

    Docs page

---
+++ docs/index.js
@@ -0,0 +1,12 @@
+/* jshint node: true, esnext: true */
+"use strict";
+
+var React = require('react');
+require('../node_modules/normalize.css/normalize.css');
+
+
+
+React.render(
+  React.createElement('div', null, 'React Context'),
+  document.getElementById('root')
+);
---
+++ gulpfile.js
@@ -0,0 +1,68 @@
+var gulp = require('gulp');
+
+var path = require('path');
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+
+
+
+config = {
+  docs: {
+    entry: ['webpack-dev-server/client?http://localhost:9100', 'webpack/hot/dev-server', './docs/index.js'],
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: 'bundle.js',
+      publicPath: 'http://localhost:9100/build/'
+    },
+    module: {
+      loaders: [
+        {
+          exclude: /node_modules/,
+          test: /\.js$/,
+          loaders: ['react-hot-loader']
+        }, {
+          test: /\.jsx$/,
+          exclude: /node_modules/,
+          loaders: ['react-hot-loader', 'jsx-loader', 'babel-loader']
+        }, {
+          test: /\.css$/,
+          loaders: [ 'style-loader', 'css-loader' ]
+        }, {
+          test: /\.md$/,
+          loaders: [ 'html-loader' ]
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['', '.js', '.jsx'],
+      fallback: [ path.resolve(__dirname, './modules') ]
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin({ quiet: true }),
+      new webpack.NoErrorsPlugin()
+    ],
+    devtool: 'eval',
+    quiet: true
+  }
+};
+
+gulp.task('docs', function(done) {
+  done();
+  return new WebpackDevServer(webpack(config.docs), {
+    publicPath: config.docs.output.publicPath,
+    hot: true,
+    stats: {
+      cached: false,
+      cachedAssets: false,
+      exclude: ['node_modules', 'components']
+    }
+  }).listen(9100, 'localhost', function(err, result) {
+    if (err) {
+      return console.log(err);
+    } else {
+      return console.log('webpack dev server listening at localhost:9100');
+    }
+  });
+});
+
+gulp.task('default', ['docs']);
